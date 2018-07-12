6491ab19dc1984365b9dd99e54792d2d3e0786e4
Christopher Pace <Christopher_pace@ymail.com>
2018-07-11 21:17:02 -0500

    Initial Coomit of HW

---
+++ Day2HW.py
@@ -0,0 +1,10 @@
+y='How does this work?"
+print(y)
+
+#SyntaxError: EOL while scanning string literal
+#This error is saying that by the End Of Line,
+# it can't find matching quotation marks
+
+
+x=["Apples","Bananas","Carrots"]
+print(*(x))
